c4b6ab561b5748ba4c386b3aa289edc7dca5ce55
Nikola Hardi <atlantic777@lugons.org>
2014-10-15 01:12:46 +0200

    Added articles list view and template

--- article_manager/views.py
+++ article_manager/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article_manager.models import Article
 
 # Create your views here.
-def article_manager_base_view(request):
-    return HttpResponse("Hello there!")
+def articles_list(request):
+    stored_articles = Article.objects.all()
+
+    dokuwiki_articles = [] #TODO: fetch'em all!
+
+    context = { 'dokuwiki_articles' : dokuwiki_articles,
+                'stored_articles' : stored_articles,
+                }
+    return render(request, 'articles_list.html', context)
--- liberator/urls.py
+++ liberator/urls.py
@@ -6,6 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'liberator.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^manager/', 'article_manager.views.article_manager_base_view'),
+    url(r'^manager/', 'article_manager.views.articles_list'),
     url(r'^admin/', include(admin.site.urls)),
 )
