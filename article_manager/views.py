4559fe06fa1278b9c54e1d69db05f873da021973
Nikola Hardi <atlantic777@lugons.org>
2014-10-15 22:03:00 +0200

    Separated settings_dist, changed dokuwiki login creds and fixed indent

--- article_manager/views.py
+++ article_manager/views.py
@@ -4,22 +4,24 @@ from article_manager.models import Article
 
 from article_manager.libre import LibreManager
 
+from django.conf import settings
+
 # Create your views here.
 def articles_list(request):
     stored_articles = Article.objects.all()
 
     dokuwiki_articles = []
 
-    remote = LibreManager("username", "password")
+    remote = LibreManager(settings.DOKUWIKI_USERNAME, settings.DOKUWIKI_PASSWORD)
     parsed_articles = remote.getAllLinked("wiki:prikupljeni_clanci")
-    for a in parsed_articles: 
-	if a.getTitle().strip() != "":
-		entry = Article()
-		entry.name = a.getTitle()
-		entry.author = a.getAuthor()
-		entry.contents_lat = a.getText() # TODO parse only contents without title, author and status, decide cyr vs lat
-		entry.source = a.getId()
-		dokuwiki_articles.append(entry)
+    for a in parsed_articles:
+        if a.getTitle().strip() != "":
+            entry = Article()
+            entry.name = a.getTitle()
+            entry.author = a.getAuthor()
+            entry.contents_lat = a.getText() # TODO parse only contents without title, author and status, decide cyr vs lat
+            entry.source = a.getId()
+            dokuwiki_articles.append(entry)
 
 
     context = { 'dokuwiki_articles' : dokuwiki_articles,
