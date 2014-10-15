75879303d73841b15ec85639601f12e8741e52cc
Stefan Nožinić <stefan@lugons.org>
2014-10-15 16:32:08 +0000

    Article list from dokuwiki
    Need installation of pydokuwiki

---
+++ article_manager/libre.py
@@ -0,0 +1,124 @@
+# coding=UTF-8
+
+from dokuwiki.parsers import Parser, LineParser
+from dokuwiki.elements import LineElement
+from dokuwiki.dokuwikixmlrpc import DokuWikiClient 
+from dokuwiki.html import HTMLParser
+
+import re
+
+class LibreTextParser(Parser): 
+	def onDocumentStart(self): 
+		self.libre_title = ""
+		self.libre_author = ""
+		self.libre_status = "U_PRIPREMI"
+	def onHeading(self, level, text):
+		if level == 6: 
+			self.libre_title = text.strip()
+	def onText(self, text):
+		
+		# do some libre-specific stuff 
+		if text.strip() in ["U_PRIPREMI", "ORIGINAL", "PRIHVACEN", "LEKTORISAN", "PROVEREN"]:
+			self.libre_status = text.strip()
+			return 
+		m = re.compile(r"(Autor|autor|autori|Autori|Аутор):? *(.*)") # check for author matching 
+		match = m.match(text) 
+		if match: 
+			self.libre_author = match.group(2)
+			return 	
+
+class LibreText(object): 
+	
+	def __init__(self, text, source = ""): 
+		
+		self.id = source
+
+		parser = LibreTextParser() 
+		blocks = text.split("\n")
+		for line in blocks: 
+			parser.parse(line) 
+		parser.finish()
+		self.title = parser.libre_title 
+		self.author = parser.libre_author 
+		self.status = parser.libre_status
+
+		html_parser = HTMLParser() 
+		for line in blocks: 
+			html_parser.parse(line)
+		html_parser.finish()
+		self.html = html_parser.getOutput()
+		self.text = text 
+
+	def isChecked(self): 
+		"""
+		Checks whether text has a status PROVEREN 
+		"""
+		return self.status == "PROVEREN"
+	
+	def getId(self): 
+		return self.id
+
+	def getTitle(self): 
+		"""
+		Returns text title
+		"""
+		return self.title 
+	
+	def getText(self): 
+		"""
+		Returns text
+		"""
+		return self.text 
+
+	def getAuthor(self): 
+		"""
+		Returns author information
+		"""
+		return self.author
+	def getStatusString(self): 
+		"""
+		Returns status as a string representation
+		"""
+		return self.status 
+	def getHTML(self): 
+		"""
+		Returns HTML of a text
+		"""
+		return self.html
+
+
+class LibreManager(object): 
+	
+	def __init__(self, username, password): 
+		self.remote = DokuWikiClient("https://libre.lugons.org/wiki", username, password)
+
+	def getPage(self, page): 
+		"""
+		Returns specified page id as a LibreText object
+
+		NOTE: page should be with namespace
+		"""
+		# try without namespace
+		l = LibreText(self.remote.page(page), page)
+		if l.getTitle() == "":
+			l = LibreText(self.remote.page("wiki:" + page), "wiki:" + page) # try with namespace 
+		return l
+	
+	def getAllLinked(self, source): 
+		"""
+		Returns a list of LibreText objects which are all pages linked from specified page
+		"""
+		res = []
+		links = self.remote.links(source) 
+		for link in links:
+			if link["type"] == "local": 
+				#print link["page"]
+				#print "getting page " + link["page"]
+				libretext = self.getPage(link["page"])
+				#print "Title: " + libretext.getTitle() 
+				#print
+				#print "Title: " + libretext.getTitle()
+				res.append(libretext)
+		return res
+
+
--- article_manager/views.py
+++ article_manager/views.py
@@ -2,11 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article_manager.models import Article
 
+from article_manager.libre import LibreManager
+
 # Create your views here.
 def articles_list(request):
     stored_articles = Article.objects.all()
 
-    dokuwiki_articles = [] #TODO: fetch'em all!
+    dokuwiki_articles = []
+
+    remote = LibreManager("username", "password")
+    parsed_articles = remote.getAllLinked("wiki:prikupljeni_clanci")
+    for a in parsed_articles: 
+	if a.getTitle().strip() != "":
+		entry = Article()
+		entry.name = a.getTitle()
+		entry.author = a.getAuthor()
+		entry.contents_lat = a.getText() # TODO parse only contents without title, author and status, decide cyr vs lat
+		entry.source = a.getId()
+		dokuwiki_articles.append(entry)
+
 
     context = { 'dokuwiki_articles' : dokuwiki_articles,
                 'stored_articles' : stored_articles,
