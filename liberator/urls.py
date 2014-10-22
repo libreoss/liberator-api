cad18f52d496df8250f9c401a21f7c923f1bc85a
Stefan Nožinić <stefan@lugons.org>
2014-10-22 18:08:54 +0000

    Article preview

--- liberator/urls.py
+++ liberator/urls.py
@@ -7,7 +7,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^manager/$', 'article_manager.views.articles_list'),
-    url(r"^manager/articles/(?P<article_id>\d+)", "article_manager.views.article_view"),
+    url(r"^manager/articles/(?P<article_id>\d+)", "article_manager.views.article_view", name="article_view"),
     url(r"^manager/import/(?P<wiki_slug>.*)?", "article_manager.views.wiki_import", name="wiki_import"),
     url(r'^admin/', include(admin.site.urls)),
 )
