301bc645d0dbf8a11302569b5ac74e9fcfd05e5b
Stefan Nožinić <stefan@lugons.org>
2014-10-25 10:10:27 +0000

    Article diff with wiki article

--- article_manager/views.py
+++ article_manager/views.py
@@ -7,6 +7,8 @@ from article_manager.libre import LibreManager
 
 from django.conf import settings
 
+from difflib import Differ
+
 # Create your views here.
 def articles_list(request):
     stored_articles = Article.objects.all()
@@ -64,6 +66,33 @@ def article_view(request, article_id):
     article = Article.objects.get(pk=int(article_id))
     context = {"article": article}
     return render(request, "article_view.html", context)
+
+def article_diff(request, article_id):
+    article = Article.objects.get(pk= int(article_id))
+    diff = [] 
+    remote = LibreManager(settings.DOKUWIKI_USERNAME, settings.DOKUWIKI_PASSWORD)
+    wiki_article = remote.getPage(article.source)
+    t1 = []
+    t2 = []
+    if wiki_article.isCyr():
+        t1 = article.contents_cyr.split("\n")
+        t2 = wiki_article.getText().split("\n")
+    else:
+        t1 = article.contents_lat.split("\n")
+        t2 = wiki_article.getText().split("\n")
+    # We remove blank lines 
+    nt1 = [] 
+    nt2 = [] 
+    for line in t1: 
+        if line.strip() != "":
+            nt1.append(line.replace("\r", ""))
+    for line in t2:
+        if line.strip() != "":
+            nt2.append(line.replace("\r", ""))
+    d = Differ()
+    diff = list(d.compare(nt1, nt2))
+    context = {"diff": diff}
+    return render(request, "article_diff.html", context)
 
 def wiki_import(request, wiki_slug):
     imported = 0
