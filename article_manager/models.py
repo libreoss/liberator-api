71a121260761a5f79172d996f3d7e8f598714561
Stefan Nožinić <stefan@lugons.org>
2014-12-25 19:41:29 +0100

    fromDokuwikiArticle method in Article model for creating Article objects from dokuwiki articles

--- article_manager/models.py
+++ article_manager/models.py
@@ -14,3 +14,24 @@ class Article(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    def fromDokuwikiArticle(article): 
+        title = article.getTitle()
+        slug = article.getId() 
+        author = article.getAuthor()
+        lat = parsed_article.getLatHTML()
+        cyr = ""
+        if article.isCyr():
+            cyr = article.getHTML()
+        entry = Article()
+        entry.name = title 
+        entry.author = author
+        if article.isCyr():
+            entry.source_cyr = slug
+            entry.source_lat = ""
+        else: 
+            entry.source_lat = slug 
+            entry.source_cyr = ""
+        entry.contents_lat = lat
+        entry.contents_cyr = cyr
+        return entry
