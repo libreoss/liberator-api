7291c034c96cc2a5cb641b3598a473b85a06f289
Stefan Nožinić <stefan@lugons.org>
2014-12-25 23:50:34 +0100

    Editor - article creation and saving to database

--- article_editor/views.py
+++ article_editor/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 from article_manager.models import Article
 
 def article_submit(request, article_id="", script=""):
-    if request.method == "GET":
-        if article_id != "": 
+    if article_id != "": 
+        if request.method == "GET":
             return render(request, "editor.html", {"article": Article.objects.get(pk=int(article_id)), "script": script})
-        return render(request, "editor.html", {})
-    else: 
-        document = request.POST["myDoc"]
-        return render(request, "show_document.html", {"doc": document})
+        else: 
+            pass # TODO Handle this case
+    else:
+        if request.method == "GET":
+            return render(request, "editor.html", {})
+        else: 
+            document = request.POST["myDoc"]
+            title = request.POST["articleTitle"]
+
+            entry = Article()
+            entry.name = title
+            entry.author = "unknown" # Get info about currently logged user
+            entry.contents_lat = document 
+            entry.save()
+            return render(request, "show_document.html", {"doc": document})
 
