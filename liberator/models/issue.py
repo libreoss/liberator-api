46e36fb7b8fc1be40c5f9b1aa1aeff9727e0344a
Andrija Prčić <bandigarf@lugons.org>
2015-01-31 22:13:22 +0100

    __str__ methods for database models

--- liberator/models/issue.py
+++ liberator/models/issue.py
@@ -6,9 +6,16 @@ from .language import Language
 class Issue(models.Model):
     publication_date = models.DateTimeField()
 
+    def __str__(self):
+        titles = IssueTitle.objects.filter(issue=self)
+        if len(titles) == 0:
+            return "???"
+        return " • ".join([title.__str__() for title in titles])
 
 class IssueTitle(models.Model):
     issue = models.ForeignKey(Issue)
     language = models.ForeignKey(Language)
     title = models.CharField(max_length=60)
 
+    def __str__(self):
+        return "{title} ({language})".format(language=self.language.name, title=self.title)
--- liberator/models/language.py
+++ liberator/models/language.py
@@ -3,3 +3,6 @@ from django.db import models
 
 class Language(models.Model):
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
