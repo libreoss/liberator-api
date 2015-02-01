8ff7e0ef7ec81148e2c703cf28dc57779bca816b
Nikola Hardi <atlantic777@lugons.org>
2015-02-01 01:15:16 +0100

    Dummy bdd runner

---
+++ bdd/steps/NewArticleSubmit.py
@@ -0,0 +1,61 @@
+from behave import *
+
+@given('that Liberator is running')
+def step_impl(context):
+    pass
+
+@given('the homepage is at http://localhost:8000')
+def step_impl(context):
+    pass
+
+@given('no access privileges are implemented')
+def step_impl(context):
+    pass
+
+@given('I load the page for new article submission')
+def step_impl(context):
+    pass
+
+@then('I should see the info "Submit a new article"')
+def step_impl(context):
+    pass
+
+@then('I should see current time and date')
+def step_impl(context):
+    pass
+
+@then('I should see "Article title" field')
+def step_impl(context):
+    pass
+
+@then('I should see "Article subtitle" field')
+def step_impl(context):
+    pass
+
+@then('I should see "Article author ID" field')
+def step_impl(context):
+    pass
+
+@then('I should see "Article body" field')
+def step_impl(context):
+    pass
+
+@then('I should see "Article script" selection with "Cyrillic" and "Latin"')
+def step_impl(context):
+    pass
+
+@then('I should see "Submit" button')
+def step_impl(context):
+    pass
+
+@given('I load the page and fill in some of the fields')
+def step_impl(context):
+    pass
+
+@when('I click on "Submit" button')
+def step_impl(context):
+    pass
+
+@then('I should see a message "The data is to be checked and saved"')
+def step_impl(context):
+    pass
