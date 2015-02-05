d804a62bac55b5c86bc46dd5e2cd354f173710e3
Goran Mekić <meka@lugons.org>
2015-02-05 16:58:32 +0100

    Be more pythonic on import

--- liberator/models/user.py
+++ liberator/models/user.py
@@ -1,7 +1,8 @@
-from django.contrib.auth.models import \
-    AbstractBaseUser, \
-    PermissionsMixin, \
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
     BaseUserManager
+)
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
--- liberator/serializers/__init__.py
+++ liberator/serializers/__init__.py
@@ -1,16 +1,11 @@
-from .article import \
-    ArticleSerializer, \
-    ArticleStateSerializer, \
-    ArticleTitleSerializer, \
+from .article import (
+    ArticleSerializer,
+    ArticleStateSerializer,
+    ArticleTitleSerializer,
     ArticleContentSerializer
-from .issue import \
-    IssueSerializer, \
-    IssueTitleSerializer
-from .user import UserSerializer
+)
+from .issue import IssueSerializer, IssueTitleSerializer
 from .language import LanguageSerializer
-from .section import \
-    SectionSerializer, \
-    SectionTitleSerializer
-from .serie import \
-    SerieSerializer, \
-    SerieTitleSerializer
+from .section import SectionSerializer, SectionTitleSerializer
+from .serie import SerieSerializer, SerieTitleSerializer
+from .user import UserSerializer
