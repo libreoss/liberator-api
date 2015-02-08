af57054d4f694bb77ca8c8ef8cd03cb99c5ec1be
Nikola Hardi <atlantic777@lugons.org>
2015-02-08 03:01:07 +0100

    Example tests with django-nose

---
+++ liberator/tests/article.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+class TestArticle(TestCase):
+    def test_article_1(self):
+        pass
+    def test_article_2(self):
+        pass
+
+class TestArticleTitle(TestCase):
+    pass
+
+class TestArticleContent(TestCase):
+    pass
+
+class TestArticleState(TestCase):
+    pass
