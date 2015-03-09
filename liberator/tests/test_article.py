d43a8b323b9df55f8592216f832987d1efdab31a
Goran Mekić <meka@lugons.org>
2015-03-09 17:57:23 +0100

    Add factory boy

--- liberator/tests/test_article.py
+++ liberator/tests/test_article.py
@@ -1,18 +1,17 @@
 import liberator.models
 from . import TestCase
+from liberator.factories import ArticleFactory, ArticleStateFactory
 
 
 class TestArticle(TestCase):
     def setUp(self):
         super(TestArticle, self).setUp()
-        self.state = liberator.models.ArticleState.objects.create(
-            name='published'
-        )
+        self.state = ArticleStateFactory()
+        self.state.save()
 
     def test_create_article(self):
-        article = liberator.models.Article.objects.create(
-            state=self.state
-        )
+        article = ArticleFactory(state=self.state)
+        article.save()
         url = '/api/v1/articles/{0}/'.format(article.id)
         article_api = self.client.get(url, **self.args)
         self.assertEqual(article.pk, article_api.data['id'])
