f2831dc7b984eefa1824c6eb716c493cddfaca9e
Goran Mekić <meka@lugons.org>
2015-03-12 01:00:48 +0100

    Include cors django app

--- project/settings/common.py
+++ project/settings/common.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'liberator',
