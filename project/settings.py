dedd9d5b23cee105bdcd89c0a4bb0c3f4f5d4299
Stefan Nožinić <stefan@lugons.org>
2016-04-30 13:05:52 +0200

    Add statis root

--- project/settings.py
+++ project/settings.py
@@ -90,6 +90,7 @@ LOGIN_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'liberator.User'
